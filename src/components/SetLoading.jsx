532d6858bce9f8509378fcb24ce92c06210fedb2
Apon Talukder <apontalukder212@gmail.com>
2025-10-08 22:41:02 +0600

    Logo Spinner

--- src/components/SetLoading.jsx
+++ src/components/SetLoading.jsx
@@ -1,13 +1,24 @@
+// import React, { memo } from "react";
+//import { ClipLoader } from "react-spinners";
+//const SetLoading = () => {
+//return (
+//<>
+//<div className="flex justify-center items-center h-60">
+//<ClipLoader color="#008cff" size={20} />
+//</div>
+//</>
+//);
+//};
+
+//export default memo(SetLoading);
 import React, { memo } from "react";
-import { ClimbingBoxLoader } from "react-spinners";
+
+import spinner from "../assets/logo.png";
 const SetLoading = () => {
   return (
-    <>
-      <div className="flex justify-center items-center h-60">
-        <ClimbingBoxLoader color="#008cff" size={20} />
-      </div>
-    </>
+    <div className="flex justify-center items-center h-60">
+      <img src={spinner} alt="Loading..." className="w-16 h-16 animate-spin" />
+    </div>
   );
 };
-
 export default memo(SetLoading);
